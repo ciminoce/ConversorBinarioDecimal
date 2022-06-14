cff9ee4b16c788335be30fb0bb4d3777a5fa109a
ciminoce <ciminoce@yahoo.com.ar>
2022-06-14 09:07:12 -0300

    Agregar archivos de proyecto.

---
+++ ConversorBinarioDecimal.Consola/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorBinarioDecimal.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número binario:");
+            var nroBinario = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{nroBinario} es  equivalente a {Conversor.ConvertirBinarioDecimal(nroBinario)}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConversorBinarioDecimal/Conversor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ConversorBinarioDecimal
+{
+    public static class Conversor
+    {
+        public static long ConvertirDecimalBinario(int numero)
+        {
+            int digitoBinario = 1;
+            int nroBinario = 0;
+            while (numero>=1)
+            {
+                if (numero % 2==1)
+                {
+                    nroBinario = digitoBinario;
+                }
+
+                numero /= 2;
+                digitoBinario *= 10;
+            }
+
+            return nroBinario;
+        }
+
+        public static int ConvertirBinarioDecimal(int numero)
+        {
+            int nroDecimal=0;
+            int posicion = 0;
+            int digitoBinario=0;
+            while (numero>0)
+            {
+                digitoBinario = numero % 10;
+                nroDecimal=nroDecimal+ (digitoBinario *(int) Math.Pow(2,posicion));
+                numero = numero / 10;
+                posicion++;
+            }
+
+            return nroDecimal;
+        }
+    }
+}
